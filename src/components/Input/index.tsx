a3f745771f6db01c3be7cc0864c300222cecf7fc
Saull Brandão <saull@outlook.com>
2021-08-03 11:44:11 -0300

    feat: creat input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+export function Input() {
+  return (
+    <input
+      type="text"
+      placeholder="Create a new todo..."
+      className="w-full h-full rounded-md pl-4 text-sm"
+    />
+  )
+}
