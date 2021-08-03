ec3778099efd8626062b64795d628a2169904b3f
Saull Brandão <saull@outlook.com>
2021-08-03 14:53:23 -0300

    feat: change the sizes of the header elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
-import MoonIcon from 'public/icon-moon.svg'
 
 export function Header() {
   return (
-    <header className="flex justify-between h-6 ">
-      <h1 className="text-2xl font-bold text-white tracking-wider">T O D O</h1>
-      <Image src="/icon-moon.svg" height={24} width={24} alt="moon icon" />
+    <header className="flex justify-between items-center">
+      <h1 className="text-3xl font-bold text-white tracking-wide">T O D O</h1>
+      <Image src="/icon-moon.svg" height={22} width={22} alt="moon icon" />
     </header>
   )
 }
