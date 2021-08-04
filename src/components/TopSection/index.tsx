4b6895884da1f8fbb3650289a5cf2a020a7da7cd
Saull Brandão <saull@outlook.com>
2021-08-04 10:37:25 -0300

    fix: set a min height to the top section so it does not shrink when the list grows

--- src/components/TopSection/index.tsx
+++ src/components/TopSection/index.tsx
@@ -3,7 +3,7 @@ import { Header } from 'components/Header'
 
 export function TopSection() {
   return (
-    <section className="flex flex-col justify-evenly px-6 bg-mobile-light bg-no-repeat bg-cover h-[30%]">
+    <section className="flex flex-col justify-evenly px-6 bg-mobile-light bg-no-repeat bg-cover min-h-[30%]">
       <Header />
       <Form />
     </section>
