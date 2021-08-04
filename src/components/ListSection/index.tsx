6b80371181bcfb22fc37fb93af854b951694908c
Saull Brandão <saull@outlook.com>
2021-08-04 09:43:59 -0300

    feat: add footer to the List Section

--- src/components/ListSection/index.tsx
+++ src/components/ListSection/index.tsx
@@ -5,11 +5,15 @@ export function ListSection() {
     <section className="flex flex-col gap-4 w-full px-6">
       <List />
 
-      <nav className="flex justify-center gap-6 bg-white w-full px-5 py-4 rounded-md shadow-md">
+      <nav className="flex justify-center gap-6 bg-white w-full px-5 py-4 rounded-md shadow-sm text-light-gray-400 font-bold">
         <a href="#">All</a>
         <a href="#">Active</a>
         <a href="#">Completed</a>
       </nav>
+
+      <footer className="text-center text-light-gray-400 font-bold text-base ">
+        Drag and drop to reorder list
+      </footer>
     </section>
   )
 }
