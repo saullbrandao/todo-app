686c4fa1569098ec95783e6d1ddd096ea34f0358
Saull Brandão <saull@outlook.com>
2021-08-04 10:39:51 -0300

    refactor: type the input element props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,9 @@
-export function Input() {
+import { InputHTMLAttributes } from 'react'
+
+export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
+      {...props}
       type="text"
       placeholder="Create a new todo..."
       className="flex-1 rounded-md text-sm py-2 outline-none"
