e8e1d596bf8bd00c1ed7007d1282ba1ee8a863a5
Saull Brandão <saull@outlook.com>
2021-08-04 09:51:48 -0300

    feat: add the checkbox component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,10 @@
+import { CheckBox } from 'components/CheckBox'
 import { Input } from 'components/Input'
 
 export function Form() {
   return (
     <form className="flex w-full h-12 rounded-md bg-white items-center gap-4 px-5 focus-within:ring-1 ring-black">
-      <span className="w-6 h-6 rounded-full  border border-light-gray-400 " />
+      <CheckBox checked={false} />
       <Input />
     </form>
   )
