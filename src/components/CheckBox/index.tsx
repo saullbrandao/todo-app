8158c8367ded3f203cefd6b897f48ae12f6ea9b4
Saull Brandão <saull@outlook.com>
2021-08-04 09:51:36 -0300

    feat: make the checkbox component accept other props

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,3 +1,3 @@
-export function CheckBox({ id = '' }) {
-  return <input id={id} type="checkbox" className="w-6 h-6" />
+export function CheckBox({ id = '', ...props }) {
+  return <input {...props} id={id} type="checkbox" className="w-6 h-6" />
 }
