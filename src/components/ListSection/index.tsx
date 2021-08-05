6dbea13998bbde7901097d8bd3316a02b0d2fcf7
Saull Brandão <saull@outlook.com>
2021-08-05 10:04:47 -0300

    feat: remove the footer message

--- src/components/ListSection/index.tsx
+++ src/components/ListSection/index.tsx
@@ -7,16 +7,11 @@ export function ListSection() {
   return (
     <section className="flex flex-col gap-4 w-full px-6">
       <List />
-
       <nav className="flex justify-center gap-6 bg-white w-full px-5 py-4 rounded-md shadow-sm text-light-gray-400 font-bold">
         <button onClick={() => handleFilter('all')}>All</button>
         <button onClick={() => handleFilter('active')}>Active</button>
         <button onClick={() => handleFilter('completed')}>Completed</button>
       </nav>
-
-      <footer className="text-center text-light-gray-400 font-bold text-base ">
-        Drag and drop to reorder list
-      </footer>
     </section>
   )
 }
