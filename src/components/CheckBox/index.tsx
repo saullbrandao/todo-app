a35bebc7d6d2312b3459bca1cd3e4cd844da033e
Saull Brandão <saull@outlook.com>
2021-08-06 09:12:35 -0300

    feat: style the checkbox to follow the app's theme

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,5 +1,29 @@
+import Image from 'next/image'
 import { InputHTMLAttributes } from 'react'
+import checkIcon from 'public/icon-check.svg'
 
-export function CheckBox(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} type="checkbox" className="w-6 h-6" />
+export function CheckBox({
+  checked,
+  id,
+  ...restProps
+}: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="relative w-6 h-6">
+      <input
+        id={id}
+        checked={checked}
+        {...restProps}
+        type="checkbox"
+        className="z-10 w-full h-full appearance-none border border-light-gray-300 dark:border-dark-gray-400 rounded-full checked:bg-gradient-to-br from-gradient-start to-gradient-end"
+      />
+      <label
+        htmlFor={id}
+        className={`absolute z-0 top-[-1px] left-[30%]  ${
+          !checked && 'hidden'
+        }`}
+      >
+        <Image src={checkIcon} alt="checkbox mark" />
+      </label>
+    </div>
+  )
 }
