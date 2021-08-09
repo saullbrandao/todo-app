20125d8c545b6f9222413d955cd066dfabe68ee9
Saull Brandão <saull@outlook.com>
2021-08-09 14:52:20 -0300

    chore: correct the jest setup to work with image imports

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png|webp)$': '<rootDir>/__mocks__/fileMock.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
 }
